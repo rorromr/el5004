4e53817b141e22969e15faf36da7908a318a3da2
Rodrigo Muñoz <rorro.mr@gmail.com>
2016-06-24 16:36:33 -0400

    Mejoras en scripts de CI

---
+++ scripts/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Source tools
+source $1/scripts/build_tool.sh
+# Build GoKart examples
+build_examples $1/arduino/GoKart
+
--- scripts/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for EXAMPLE in $PWD/arduino/GoKart/examples/*/*.ino; do
-	arduino --verify --board arduino:avr:mega:cpu=atmega2560 $EXAMPLE
-done
