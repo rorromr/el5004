f59ac95552b2ba15c35fa387367d6cd6eae6564b
Rodrigo Muñoz <rorro.mr@gmail.com>
2016-07-06 01:08:16 -0400

    Basic funtions for LCD menu

--- arduino/GoKart/lcd.cpp
+++ arduino/GoKart/lcd.cpp
@@ -1,15 +1,86 @@
 #include "lcd.h"
+#include "Arduino.h"
 
 namespace GoKart
 {
-  LCD::LCD()
-  {
-    ;
-  }
+  LCD::LCD():
+    lcd_(GOKART_LCD_RS, GOKART_LCD_EN, GOKART_LCD_D0, GOKART_LCD_D1, GOKART_LCD_D2, GOKART_LCD_D3),
+    servoCount_(0U),
+    servoSelected_(0U)
+  {}
 
   bool LCD::init()
   {
+    lcd_.begin(GOKART_LCD_COLS, GOKART_LCD_ROWS);
     return true;
+  }
+
+  ButtonState LCD::getButton()
+  {
+    // Read the value from the sensor
+    uint16_t adc_val = analogRead(GOKART_LCD_BUTTON_PIN);
+
+    if (adc_val > 1000U) return BTN_NONE;
+    if (adc_val < 50U)   return BTN_RIGHT;  
+    if (adc_val < 250U)  return BTN_UP; 
+    if (adc_val < 450U)  return BTN_DOWN; 
+    if (adc_val < 650U)  return BTN_LEFT; 
+    else return BTN_SELECT; //(adc_val < 850U)
+  }
+
+  bool LCD::addServo(DxlServo *s, const char *name)
+  {
+    if (strlen(name)>4) return false;
+    if (s == NULL) return false;
+    ServoInfo* servo = &servoInfo_[servoCount_++];
+    
+    servo->servo = s; // Set servo ptr
+    strcpy(servo->name, name); // Set name
+    servo->pos = 0U;
+    servo->cwLimit = 0U;
+    servo->ccwLimit = 0U;
+  }
+
+  void LCD::print()
+  {
+    if (servoCount_ == 0U) return;
+
+    ButtonState btn = getButton();
+
+    switch(btn)
+    {
+      case BTN_LEFT:
+        servoSelected_ = (servoSelected_+1U) > (servoCount_-1U) ? servoSelected_ : (servoSelected_+1U);
+        break;
+      case BTN_RIGHT:
+        servoSelected_ = servoSelected_ > 0 ? (servoSelected_-1U) : 0U;
+        break;
+    }
+
+    ServoInfo* servo = &servoInfo_[servoSelected_];
+    // Update values
+    servo->pos = servo->servo->getPosition();
+    servo->cwLimit = servo->servo->getPosition();// @TODO
+    servo->ccwLimit = servo->servo->getPosition();// @TODO
+    
+    // First row: Name and position
+    lcd_.setCursor(0, 0);
+    lcd_.print(servo->name);
+    lcd_.setCursor(5, 0);
+    lcd_.print("Pos:");
+    lcd_.setCursor(9, 0);
+    lcd_.print(servo->pos);
+    // Second row: CW and CCW limit
+    lcd_.setCursor(0, 1);
+    lcd_.print("CW:");
+    lcd_.setCursor(3, 1);
+    lcd_.print(servo->cwLimit);
+    lcd_.setCursor(8, 1);
+    lcd_.print("CCW:");
+    lcd_.setCursor(11, 1);
+    lcd_.print(servo->ccwLimit);
+    
+
   }
 
 }
--- arduino/GoKart/lcd.h
+++ arduino/GoKart/lcd.h
@@ -11,15 +11,62 @@
 #define GOKART_LCD_H
 
 #include "LiquidCrystal.h"
+#include "dxl_servo.h"
+
+#define GOKART_LCD_BUTTON_PIN A4
+
+#define GOKART_LCD_RS 20
+#define GOKART_LCD_EN 20
+#define GOKART_LCD_D0 20
+#define GOKART_LCD_D1 20
+#define GOKART_LCD_D2 20
+#define GOKART_LCD_D3 20
+
+#define GOKART_LCD_COLS 16
+#define GOKART_LCD_ROWS 2
+
+#define GO_KART_LCD_MAX_SERVO 3
 
 namespace GoKart
 {
+
+  typedef struct ServoInfo_
+  {
+    DxlServo *servo;
+    char name[4];
+    uint16_t pos;
+    uint16_t cwLimit;
+    uint16_t ccwLimit;
+  } ServoInfo;
+
+  typedef enum
+  {
+    BTN_NONE   = 0U,
+    BTN_UP     = 1U,
+    BTN_LEFT   = 2U,
+    BTN_RIGHT  = 3U,
+    BTN_DOWN   = 4U,
+    BTN_SELECT = 5U
+  } ButtonState;
+
   class LCD
   {
     public:
       LCD();
 
       bool init();
+
+      ButtonState getButton();
+
+      bool addServo(DxlServo *s, const char *name);
+
+      void print();
+
+    private:
+      LiquidCrystal lcd_;
+      uint8_t servoCount_;
+      uint8_t servoSelected_;
+      ServoInfo servoInfo_[GO_KART_LCD_MAX_SERVO];
 
   };
 }
